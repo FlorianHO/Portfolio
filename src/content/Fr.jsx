1115d9202cafa123aa7346da5b2ba2d659f03018
Florian <92437122+FlorianHO@users.noreply.github.com>
2022-07-08 17:01:59 -0400

    Delete src directory

--- src/content/Fr.jsx
+++
@@ -1,7 +0,0 @@
-export default {
-  load: "Chargement...",
-  error404: ["Erreur 404 (Page non trouvée)⚠️", "Désolé, la page n'a pas pu être trouvée · Erreur 404", "Erreur 404"],
-  links: ["Accueil", "Portfolio", "API", "Terre", "Contact"],
-  indexPresentation: ["Salut !", "Je m'appelle Florian", "COMPÉTENCES", "EXPÉRIENCE PROFESSIONNELLE", "FORMATIONS", "Accéder aux compétences", "Accèder à l'expérience professionnelle ", "Accèder aux formations", "Site Portfolio", "- Application mobile - Logiciel - Maquette réseau - Application web - Systèmes embarqués - Base de données - PWA", "Je suis étudiant en 2ème année de prépa intégrée informatique.", "Je cherche", "Et "],
-  footer: "© Florian HOUDU | 2022 - Tous droits réservés"
-};
