94f686da03de216062ea6abc61a8fb08cde7bae1
Florian <92437122+FlorianHO@users.noreply.github.com>
2022-07-11 12:09:44 -0400

    Delete src directory

--- src/content/En.jsx
+++
@@ -1,4 +0,0 @@
-export default {
-    links: ["Home", "Portfolio", "API", "Earth", "Contact"],
-    indexPresentation: ["Hey!", "I'm Florian", "SKILLS", "EXPÉRIENCE PROFESSIONNELLE", "FORMATIONS", "Skip to skills", "Skip to professional experiences", "Skip to studies", "Portfolio Website", "- Mobile application - Software - Network subsystem - Web application - Embedded systems - Database - PWA", "I'm a student", "I'm looking for", "And "]
-};
