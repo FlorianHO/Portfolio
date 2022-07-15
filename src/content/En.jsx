50ada893be4a78d50989b1ea5575cb4890ccd666
Florian <92437122+FlorianHO@users.noreply.github.com>
2022-07-15 10:54:06 -0400

    Delete src directory

--- src/content/En.jsx
+++
@@ -1,6 +0,0 @@
-export default {
-    links: ["Home", "Portfolio", "API", "Earth", "Contact"],
-    indexPresentation: ["Hey!", "I'm Florian", "SKILLS", "PROFESSIONAL EXPERIENCE", "STUDIES", "Skip to skills", "Skip to professional experiences", "Skip to studies", "Portfolio Website", "- Mobile application - Software - Network subsystem - Web application - Embedded systems - Database - PWA", "I'm a student", "I'm looking for", "And ", "See my resume"],
-    footer: "All rights reserved",
-    contact: ["Send"]
-};
--- src/content/Fr.jsx
+++
@@ -1,6 +0,0 @@
-export default {
-  links: ["Accueil", "Portfolio", "API", "Terre", "Contact"],
-  indexPresentation: ["Salut !", "Je m'appelle Florian", "COMPÉTENCES", "EXPÉRIENCE PROFESSIONNELLE", "FORMATIONS", "Accéder aux compétences", "Accèder à l'expérience professionnelle ", "Accèder aux formations", "Site Portfolio", "- Application mobile - Logiciel - Maquette réseau - Application web - Systèmes embarqués - Base de données - PWA", "Je suis étudiant en 2ème année de prépa intégrée informatique.", "Je cherche", "Et", "Voir mon CV"],
-  footer: "Tous droits réservés",
-  contact: ["Envoyer"]
-};
